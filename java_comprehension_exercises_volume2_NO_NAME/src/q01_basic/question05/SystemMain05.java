9a072a65f1548aa344a9f4187f84ab8a161166ea
bisuma <bisuma@C010423070.zipangu.cac.co.jp>
2026-04-28 16:09:34 +0900

    0428 q01_basic.question05まで完了
    理解できた点
    ・フィールドをprivateにして getter/setterを通じてアクセスするカプセル化の実装方法を理解した。
    ・抽象メソッドをオーバーライドすることで、ポリモーフィズムを実現し、適切なメソッドを呼び出す仕組みを理解した。
    
    不明な点、調査中
    ・メソッド内でtry-catchするべきか、throwsで呼び出し元に投げるべきかの判断基準を学びたい。
    ・staticメソッドにする場合と、インスタンス化して利用する場合の違いについて学習中である。

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/SystemMain05.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/SystemMain05.java
@@ -3,10 +3,24 @@ package q01_basic.question05;
 public class SystemMain05 {
 
 	public static void main(String[] args) {
+		System.out.println("---SHOW MEMBERS--- ");
 
 		AbstMember[] members = new AbstMember[2];
-		//TODO ここから実装する
 
+		AbstMember nonmember = new NonMember("Sato Kensuke");
+		members[0] = nonmember;
+
+		AbstMember member = new Member(1, "PassW0rd", "Miura Manabu", 28, 2);
+		members[1] = member;
+
+		AbstMember[] m = { nonmember, member };
+
+		MemberManager.showAllMembers(m);
+
+		System.out.println("---BUY ITEM--- ");
+		for (AbstMember abst : m) {
+			abst.buyItem();
+		}
 	}
 
 }
