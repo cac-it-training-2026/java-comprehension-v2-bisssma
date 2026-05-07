ad90604425930a2255dc5c0785d95a5ec9a35358
bisuma <bisuma@C010423070.zipangu.cac.co.jp>
2026-05-07 12:33:02 +0900

    0507 q02_advanced.question01-03まで完了
    理解できた点
    ・メソッドの引数内で直接newをするのではなく、一度変数（coupon1など）で受け取ってからリストに追加すること。
    ・データを持つだけのクラスと実際の作業を行うクラスが、どのように協力して1つのシステムとして動いているかの理解が深まった。
    
    不明な点
    ・getInstanceのようなstaticメソッドの中では、インスタンス自身を指すthisは使えないルールの整理がもう少し必要。

--- java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question02/SystemMain02.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question02/SystemMain02.java
@@ -1,11 +1,43 @@
 package q02_advanced.question02;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class SystemMain02 {
 
 	public static void main(String[] args) {
 
 		//TODO ここから実装する
+		ConsoleReader cr = new ConsoleReader();
+		ArrayList<Member> members = new ArrayList<>();
 
+		Member member1 = Member.getInstance(1, "Password", "Miura Manabu", 28, 2);
+		members.add(member1);
+
+		Member member2 = Member.getInstance(2, "aaaAAA", "Sato Kensuke", 43, 1);
+		members.add(member2);
+
+		System.out.println("===会員情報を表示します===");
+		MemberManager.showAllMembers(members);
+
+		System.out.println("===パスワードを変更します===");
+
+		try {
+			System.out.print("input target id >>");
+			int targetId = cr.inputNumber();
+			System.out.print("input new password >>");
+			String newPassword = cr.inputString();
+
+			MemberManager.updatePassword(members, targetId, newPassword);
+
+		} catch (IOException | NumberFormatException e) {
+			System.out.println("不正な入力です");
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println("---SHOW DATA---");
+		MemberManager.showAllMembers(members);
 	}
 
 }
--- java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question03/LoginService.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q02_advanced/question03/LoginService.java
@@ -1,5 +1,7 @@
 package q02_advanced.question03;
 
+import java.util.List;
+
 /**
  * MemberStorageにアクセスし、ログイン処理を行う
  */
@@ -11,4 +13,20 @@ class LoginService {
 	private MemberStorage memberStorage;
 
 	//TODO ここから処理を記述
+	public LoginService(MemberStorage memberStorage) {
+		this.memberStorage = memberStorage;
+	}
+
+	public Member doLogin(int id, String password) {
+		List<Member> members = memberStorage.getMembers();//ただ元々ある情報を取ってくるだけ
+
+		if (members != null) {//当たり前にnullじゃないのでそのままfor文へ
+			for (Member member : members) {//「元々の情報」と「入力した情報」の比較
+				if (member.getId() == id && member.getPassword().equals(password)) {
+					return member;
+				}
+			}
+		}
+		return null;
+	}
 }
